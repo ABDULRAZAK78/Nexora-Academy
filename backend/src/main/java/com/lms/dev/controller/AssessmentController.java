c692579e5a5df31ebcb46ae5a61fe0b8dc964891
Abdulrazak27 <2300032428@kluniversity.in>
2026-05-06 11:00:14 +0530

    Restore Autowired imports

--- backend/src/main/java/com/lms/dev/controller/AssessmentController.java
+++ backend/src/main/java/com/lms/dev/controller/AssessmentController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import com.lms.dev.entity.Assessment;
 import com.lms.dev.entity.Course;
@@ -59,6 +60,7 @@ public class AssessmentController {
         return assessmentService.saveAssessment(user , course, assessment);
     }
 }
+
 
 
 
